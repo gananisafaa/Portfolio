9677fcb2315431dd57bc2ff342e80ebd56df5eed
gananisafaa <ganani.sg@gmail.com>
2021-05-04 02:19:14 +0000

    Deploying to gh-pages from  @ a63a34070c5cffb2cdc55ffabe572e7e434a4e65 🚀

---
+++ precache-manifest.101491605cb8fff3d38ad85fe4fa9eb7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ac7b35c663bb1ddc54056b70aea629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b341b342b1941f76ced",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "7b9d06f9fa60f47d8713",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "b10b657cef7257f0aa41",
+    "url": "/static/css/main.f4d5d5da.chunk.css"
+  },
+  {
+    "revision": "dbbf40b847fb058efc79",
+    "url": "/static/js/2.0d2518a8.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.0d2518a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b341b342b1941f76ced",
+    "url": "/static/js/3.a3d01360.chunk.js"
+  },
+  {
+    "revision": "7b9d06f9fa60f47d8713",
+    "url": "/static/js/4.74a80a00.chunk.js"
+  },
+  {
+    "revision": "b10b657cef7257f0aa41",
+    "url": "/static/js/main.8217fb46.chunk.js"
+  },
+  {
+    "revision": "d02e1146a9674888800a",
+    "url": "/static/js/runtime-main.d0e11266.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e12be258ed7acfb1427ef57e810cb574",
+    "url": "/static/media/ai.e12be258.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b97b6ac216798a71361e23fb8a304339",
+    "url": "/static/media/devops.b97b6ac2.png"
+  },
+  {
+    "revision": "216563deb0118eb1fcedbe50949aead4",
+    "url": "/static/media/ds.216563de.png"
+  },
+  {
+    "revision": "656e5b482f69beca42e7934aa7718f7a",
+    "url": "/static/media/emsi.656e5b48.png"
+  },
+  {
+    "revision": "33d8fc8a22a5978b3b364e14a8b8aed7",
+    "url": "/static/media/firethunder.33d8fc8a.png"
+  },
+  {
+    "revision": "32e035c092a841b0c6ab3fd7511b883c",
+    "url": "/static/media/gst.32e035c0.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b1ce8ce1cadaa442dcd2cc62d67acdd3",
+    "url": "/static/media/quiz.b1ce8ce1.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "87a29e70ec7862e34a4ca75149e4fb33",
+    "url": "/static/media/staff.87a29e70.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "4d03f85eea53d6918c517dfe8e67aaf9",
+    "url": "/static/media/tictactoe.4d03f85e.jpg"
+  }
+]);
